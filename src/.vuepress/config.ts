e545cf87b6f3001a45221c12e6cc6e9020bdcd8d
pengzhendong <275331498@qq.com>
2022-11-11 17:11:08 +0800

    remove english

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,23 @@
+import { defineUserConfig } from "vuepress";
+import { docsearchPlugin } from "@vuepress/plugin-docsearch";
+import theme from "./theme.js";
+
+export default defineUserConfig({
+  base: "/docs/",
+
+  plugins: [
+    docsearchPlugin({}),
+  ],
+
+  locales: {
+    "/": {
+      lang: "zh-CN",
+      title: "文档演示",
+      description: "vuepress-theme-hope 的文档演示",
+    },
+  },
+
+  theme,
+
+  shouldPrefetch: false,
+});
